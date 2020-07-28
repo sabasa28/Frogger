f8d7c60bb18e2d8259e05928ca83e5ac70f66207
Iñaki Diez Galarza <inakidiezgalarza@gmail.com>
2020-07-28 20:11:04 -0300

    Completado main menu y creditos

---
+++ Frogger IDG/Assets/Scripts/UI/UIMainMenu.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class UIMainMenu : MonoBehaviour
+{
+    public GameObject credits;
+    public void LoadGameplay()
+    {
+        SceneManager.LoadScene(2);
+    }
+    public void ExitGame()
+    {
+        Application.Quit();
+        Debug.Log("Game Closed");
+    }
+    public void ShowCredits()
+    {
+        credits.SetActive(true);
+    }
+    public void HideCredits()
+    { 
+        credits.SetActive(false);
+    }
+}
