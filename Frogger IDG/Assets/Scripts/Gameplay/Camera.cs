403f4e38fcf35177e09462d5d40ca3b4b604edb5
Iñaki Diez Galarza <inakidiezgalarza@gmail.com>
2020-07-28 23:55:57 -0300

    Hecho movimiento de rana y seguimiento de camara

---
+++ Frogger IDG/Assets/Scripts/Gameplay/Camera.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Camera : MonoBehaviour
+{
+    Transform player;
+    void Start()
+    {
+        player = FindObjectOfType<Player>().transform;
+    }
+
+    void Update()
+    {
+        if (transform.position.y != player.position.y)
+            transform.position = new Vector3(transform.position.x, player.position.y, transform.position.z);
+    }
+}
---
+++ Frogger IDG/Assets/Scripts/Gameplay/Player.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public float jumpTime;
+    enum State
+    { 
+        jumping,
+        idle,
+        dead
+    }
+    State frogState = State.idle;
+    SpriteRenderer sr;
+    public Sprite idleSprite;
+    public Sprite jumpingSprite1;
+    public Sprite jumpingSprite2;
+    Quaternion facingUp = Quaternion.Euler(new Vector3(0, 0, 0));
+    Quaternion facingDown = Quaternion.Euler(new Vector3(0, 0, 180));
+    Quaternion facingRight = Quaternion.Euler(new Vector3(0, 0, -90));
+    Quaternion facingLeft = Quaternion.Euler(new Vector3(0, 0, 90));
+
+    void Start()
+    {
+        sr = GetComponent<SpriteRenderer>();
+    }
+    void Update()
+    {
+        int movementH = (int)Input.GetAxisRaw("Horizontal");
+        int movementV = (int)Input.GetAxisRaw("Vertical");
+        Vector2 direction = Vector2.zero;
+        if (movementH != 0) direction.x = movementH;
+        else direction.y = movementV;
+        if (frogState == State.idle && direction != Vector2.zero) StartCoroutine(Jump(direction));
+    }
+    IEnumerator Jump(Vector3 direction)
+    {
+        sr.sprite = jumpingSprite1;
+        frogState = State.jumping;
+
+        if (direction == Vector3.up) transform.rotation = facingUp;
+        else if (direction == Vector3.right) transform.rotation = facingRight;
+        else if (direction == Vector3.left) transform.rotation = facingLeft;
+        else if (direction == Vector3.down) transform.rotation = facingDown;
+
+        Vector3 targetPosition = transform.position + direction;
+        Vector3 origPos = transform.position;
+        float t = 0;
+        while (transform.position != targetPosition)
+        {
+            UpdateJumpSprite(t);
+            t += Time.deltaTime / jumpTime;
+            transform.position = Vector3.Lerp(origPos, targetPosition,t);
+            
+            yield return null;
+        }
+        sr.sprite = idleSprite;
+        frogState = State.idle;
+    }
+
+    void UpdateJumpSprite(float t)
+    {
+        if (CheckMidjump(t))
+        {
+            if (sr.sprite != jumpingSprite2) sr.sprite = jumpingSprite2;
+        }
+        else if (sr.sprite != jumpingSprite1) sr.sprite = jumpingSprite1;
+    }
+
+    bool CheckMidjump(float t)
+    {
+        float minForMidJump = 0.3f;
+        float maxForMidJump = 0.7f;
+        return (t > minForMidJump && t < maxForMidJump);
+    }
+}
